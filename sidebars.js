63e684dd6e1889ae6ef0b88cfc47c522757a9dc4
bsmth <brian@smith.berlin>
2021-05-17 19:03:29 +0200

    docs: minor edit

--- sidebars.js
+++ sidebars.js
@@ -93,7 +93,7 @@ module.exports = {
       ],
     },
     {
-      label: "Management",
+      label: "Operations",
       type: "category",
       items: [
         "operations/capacity-planning",
